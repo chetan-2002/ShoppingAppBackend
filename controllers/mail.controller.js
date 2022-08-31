5cd42b704de274d66c6ef314a76526047de729f8
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-08-31 18:03:53 +0530

    added controller for sending mails on order confirmation

---
+++ controllers/mail.controller.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+
+//desc - send mail to user
+//route - POST /api/sendmail/
+const sendmail = async (req, res) => {
+  const { email, html } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "mealmonkey001@gmail.com",
+      pass: "cbwdvbbjblhfmheq",
+    },
+  });
+
+  const mailOptions = {
+    from: "mealmonkey001@gmail.com",
+    to: `${email}`,
+    subject: "Order Confirmation",
+    html: `${html}`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Error in sending mail",
+      });
+    } else {
+      //   console.log("Email sent: " + info.response);
+      res.status(200).json({
+        message: "Email sent Successfully",
+      });
+    }
+  });
+};
+
+module.exports = {
+  sendmail,
+};
