5a34019a29e2adbea2000bbb877a4ff4911c458d
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-08-31 17:51:13 +0530

    added middleware for authenticating user based on jwt token

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // req.user = await User.findById(decoded.id).select("-password");
+      // console.log(req.user);
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+};
+
+module.exports = protect;
