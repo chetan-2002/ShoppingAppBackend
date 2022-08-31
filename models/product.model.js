324b554c3cc76403b54a6819a84ca8d980e32297
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-08-31 17:55:58 +0530

    added product model

---
+++ models/product.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    size: [
+      {
+        type: Object,
+      },
+    ],
+    crust: [
+      {
+        type: Object,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
