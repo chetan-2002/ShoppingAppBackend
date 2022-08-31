46552a076f6bcb0aaf6abfea5dd51af54f1b1898
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-08-31 18:04:41 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connect = require("./config/db");
+const colors = require("colors");
+const Userroutes = require("./routes/user.routes");
+const Productroutes = require("./routes/product.route");
+const Categoryroutes = require("./routes/categories.route");
+const Cartroutes = require("./routes/cart.route");
+const Orderroutes = require("./routes/order.route");
+const Mailroutes = require("./routes/sendmail.route");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+connect();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/user", Userroutes);
+app.use("/api/product", Productroutes);
+app.use("/api/category", Categoryroutes);
+app.use("/api/cart", Cartroutes);
+app.use("/api/order", Orderroutes);
+app.use("/api/sendmail", Mailroutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`.green.bgMagenta);
+});
