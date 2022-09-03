190d2509ec7faa1bf44818067fb689d8a488c125
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-09-04 01:14:34 +0530

    added token controller

---
+++ controllers/token.controller.js
@@ -0,0 +1,25 @@
+const Token = require("../models/token.model");
+const User = require("../models/user.model");
+const mongoose = require("mongoose");
+
+//Description: Verify token
+//Route: POST /api/token/verifyToken
+//Access: Public
+const verifyToken = async (req, res) => {
+  let { token } = req.body;
+  const tokenData = await Token.findOne({ token });
+  if (tokenData) {
+    const user = await User.findOneAndUpdate(
+      { email: tokenData.email },
+      { isEmailVerified: true },
+      { new: true }
+    );
+    res.status(200).json({ message: "Token verified Successfully", user });
+  } else {
+    res.status(400).json({ err: "Invalid token" });
+  }
+};
+
+module.exports = {
+  verifyToken,
+};
